5cf0ec93d6f41ba513aa9f7f9aef1d8ab461dada
Mahlet Neguss <mahibr@yamil.com>
2023-10-18 19:35:19 -0400

    build error fix

--- src/components/Bookmarks.js
+++ src/components/Bookmarks.js
@@ -10,7 +10,7 @@ export default function Bookmarks() {
         return allBookmarks.includes(book.ISBN);
       })
     );
-  }, []);
+  }, [allBookmarks]);
   return (
     <>
       {" "}
--- src/components/ListOfBooks.jsx
+++ src/components/ListOfBooks.jsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { useState, useEffect } from "react";
 import styled from "@emotion/styled";
-import { useNavigate, useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import SearchBar from "./SearchBar";
 import axios from "axios";
 
